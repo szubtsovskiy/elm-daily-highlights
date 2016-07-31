9508f2423c60471e77bdf006f5ff5041b9cd6b42
Sergey Zubtsovskiy <sergey.zubtsovskiy@gmail.com>
2016-07-31 14:41:20 +0200

    Use LocalStorage to save an input value

--- src/App.elm
+++ src/App.elm
@@ -2,26 +2,40 @@ module App exposing (main)
 
 import Html exposing (..)
 import Html.App as Html
+import Html.Attributes exposing (..)
+import Html.Events exposing (onInput, onClick)
 import LocalStorage
+import Task
 
-main = Html.beginnerProgram {model = model, view = view, update = update}
+main = Html.program {init = init, view = view, update = update, subscriptions = (\x -> Sub.none)}
 
 type alias Model = String
 
-model : Model
-model =
-  "No"
+type Action = Error LocalStorage.Error | Highlight String | Save | NoOp
 
-type Action = NoOp
+update : Action -> Model -> (Model, Cmd Action)
+update action model =
+  case action of
+    NoOp ->
+      (model, Cmd.none)
+    Error err ->
+      let
+        _ =
+          Debug.log "Error: " (toString err)
+      in
+        (model, Cmd.none)
+    Highlight value ->
+      (value, Cmd.none)
+    Save ->
+      ("", Task.perform Error (always NoOp) (LocalStorage.set "highlights" model))
 
 view : Model -> Html Action
 view model =
   div []
-  [ text "Hi there!"
+  [ input [ type' "text", placeholder "Highlight", onInput Highlight, value model ] []
+  , button [ type' "submit", onClick Save ] [ text "save" ]
   ]
 
-update : Action -> Model -> Model
-update action model =
-  case action of
-    NoOp ->
-      model
+init : (Model, Cmd Action)
+init =
+  ("", Cmd.none)
