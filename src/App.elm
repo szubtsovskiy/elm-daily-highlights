2b7799da30ae05ec4471531f2c6060baaf86191a
Sergey Zubtsovskiy <sergey.zubtsovskiy@gmail.com>
2016-07-31 13:47:56 +0200

    App basics

---
+++ src/App.elm
@@ -0,0 +1,26 @@
+module App exposing (main)
+
+import Html exposing (..)
+import Html.App as Html
+
+main = Html.beginnerProgram {model = model, view = view, update = update}
+
+type alias Model = String
+
+model : Model
+model =
+  "No"
+
+type Action = NoOp
+
+view : Model -> Html Action
+view model =
+  div []
+  [ text "Hi there!"
+  ]
+
+update : Action -> Model -> Model
+update action model =
+  case action of
+    NoOp ->
+      model
